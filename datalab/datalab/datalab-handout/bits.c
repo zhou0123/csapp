1e5bd0592c85d078b7ed0b2a057611840ad8b833
zhou0123 <812986561@gmail.com>
2022-10-15 12:50:53 +0000

    week2

--- datalab/datalab/datalab-handout/bits.c
+++ datalab/datalab/datalab-handout/bits.c
@@ -261,7 +261,21 @@ int logicalNeg(int x) {
  *  Rating: 4
  */
 int howManyBits(int x) {
-  
+  int b16,b8,b4,b2,b1,b0;
+  int sign = 0x1>>31;
+  x = (sign&~x)|(~sign&x);
+  b16 = !!(x>>16)<<4;
+  x = x>>b16;
+  b8 = !!(x>>8)<<3;
+  x = x>>b8;
+  b4 = !!(x>>4)<<2;
+  x = x>>b4;
+  b2 = !!(x>>2)<<1;
+  x = x>>b2;
+  b1 = !!(x>>1);
+  x = x>>b1;
+  b0 = x;
+  return b16+b8+b4+b2+b1+b0+1;  
 }
 //float
 /* 
