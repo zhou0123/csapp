d3610bdc17bc67018ce58cd06d22746dc95767d2
zhou0123 <zhou812986561@gmail.com>
2022-10-20 12:53:32 +0000

    bomb

--- bomblab/bomblab/bomb/bomb.s
+++ bomblab/bomblab/bomb/bomb.s
@@ -540,7 +540,7 @@ Disassembly of section .text:
   40114b:	7e e8                	jle    401135 <phase_6+0x41>
   40114d:	49 83 c5 04          	add    $0x4,%r13
   401151:	eb c1                	jmp    401114 <phase_6+0x20>
-  401153:	48 8d 74 24 18       	lea    0x18(%rsp),%rsi
+  401153:	48 8d 74 24 18       	lea    0x18(%rsp),%rsi #上面都在check
   401158:	4c 89 f0             	mov    %r14,%rax
   40115b:	b9 07 00 00 00       	mov    $0x7,%ecx
   401160:	89 ca                	mov    %ecx,%edx
@@ -548,7 +548,7 @@ Disassembly of section .text:
   401164:	89 10                	mov    %edx,(%rax)
   401166:	48 83 c0 04          	add    $0x4,%rax
   40116a:	48 39 f0             	cmp    %rsi,%rax
-  40116d:	75 f1                	jne    401160 <phase_6+0x6c>
+  40116d:	75 f1                	jne    401160 <phase_6+0x6c>#上面都在7-
   40116f:	be 00 00 00 00       	mov    $0x0,%esi
   401174:	eb 21                	jmp    401197 <phase_6+0xa3>
   401176:	48 8b 52 08          	mov    0x8(%rdx),%rdx
@@ -1739,3 +1739,118 @@ Disassembly of section .fini:
   4022a4:	48 83 ec 08          	sub    $0x8,%rsp
   4022a8:	48 83 c4 08          	add    $0x8,%rsp
   4022ac:	c3                   	retq   
+
+
+
+00000000004010f4 <phase_6>:
+  4010f4:   41 56                   push   %r14
+  4010f6:   41 55                   push   %r13
+  4010f8:   41 54                   push   %r12
+  4010fa:   55                      push   %rbp
+  4010fb:   53                      push   %rbx
+  4010fc:   48 83 ec 50             sub    $0x50,%rsp !# 开了80个字节
+  401100:   49 89 e5                mov    %rsp,%r13
+  401103:   48 89 e6                mov    %rsp,%rsi
+  401106:   e8 51 03 00 00          callq  40145c <read_six_numbers>  !# 读出六个int
+  40110b:   49 89 e6                mov    %rsp,%r14                  !# rsp0
+
+  40110e:   41 bc 00 00 00 00       mov    $0x0,%r12d            !# 初始化
+                !# 检测六个int是否为[1,2,3,4,5,6]的任意顺序     r12d 是下标  
+  401114:   4c 89 ed                mov    %r13,%rbp             !# rsp0
+  401117:   41 8b 45 00             mov    0x0(%r13),%eax        !# 每一个一个int number 
+  40111b:   83 e8 01                sub    $0x1,%eax             !# number --  下面要unsigned 比较，所以不能<= 0
+  40111e:   83 f8 05                cmp    $0x5,%eax             !# number <= 5 就跳过下一行  说明原输入 <= 6  且 > 0 即[1,6]
+  401121:   76 05                   jbe    401128 <phase_6+0x34>
+  401123:   e8 12 03 00 00          callq  40143a <explode_bomb>
+  401128:   41 83 c4 01             add    $0x1,%r12d            !# r12d ++
+  40112c:   41 83 fc 06             cmp    $0x6,%r12d            !# 循环六次 
+  401130:   74 21                   je     401153 <phase_6+0x5f>
+  401132:   44 89 e3                mov    %r12d,%ebx            !# 下一个数
+         !# 二重循环， 检测后面的数
+    401135: 48 63 c3                movslq %ebx,%rax             
+    401138: 8b 04 84                mov    (%rsp,%rax,4),%eax    !# rsp[4 * ebx] -> eax
+    40113b: 39 45 00                cmp    %eax,0x0(%rbp)        !# 每一个数 与后面的数进行比较，相等就爆炸
+    40113e: 75 05                   jne    401145 <phase_6+0x51>
+    401140: e8 f5 02 00 00          callq  40143a <explode_bomb>
+    401145: 83 c3 01                add    $0x1,%ebx            !# 枚举后面的数
+    401148: 83 fb 05                cmp    $0x5,%ebx
+    40114b: 7e e8                   jle    401135 <phase_6+0x41>
+  40114d:   49 83 c5 04             add    $0x4,%r13         !# 枚举下一个数
+  401151:   eb c1                   jmp    401114 <phase_6+0x20>
+
+  401153:   48 8d 74 24 18          lea    0x18(%rsp),%rsi  !# rsp24
+  401158:   4c 89 f0                mov    %r14,%rax        !# rsp0
+  40115b:   b9 07 00 00 00          mov    $0x7,%ecx
+            !# cur = 7 - cur, 做六次这个循环      将之前读入的int 取 7 - int 的 值
+  401160:   89 ca                   mov    %ecx,%edx         
+  401162:   2b 10                   sub    (%rax),%edx      !# edx -= 参数 
+  401164:   89 10                   mov    %edx,(%rax)      !# 参数 = edx
+  401166:   48 83 c0 04             add    $0x4,%rax        !# 上移
+  40116a:   48 39 f0                cmp    %rsi,%rax        !# 直到把六个int都改变了
+  40116d:   75 f1                   jne    401160 <phase_6+0x6c>
+
+  40116f:   be 00 00 00 00          mov    $0x0,%esi
+  401174:   eb 21                   jmp    401197 <phase_6+0xa3> 
+
+    !# 这一段是 将 第 7 - int个链表元素 放到 rsp0x20 + int的相对位置 * 2 的位置
+    !# 下面是这个链表中node的原型
+    !#   struct node{   // 在这里需要注意的是， 虽然value是int ，但是因为64位对齐， 所以我们在访问时仍然是 +8 
+    !#      int value;
+    !#       node* next;
+    !#  };
+                    !# 寻找 第 7 - int个链表元素 
+  401176:   48 8b 52 08             mov    0x8(%rdx),%rdx  !# rdx = *rdx + 8  这里是 p = p -> next 
+  40117a:   83 c0 01                add    $0x1,%eax
+  40117d:   39 c8                   cmp    %ecx,%eax    !# 比较这个差值，按差值的倍数 加
+  40117f:   75 f5                   jne    401176 <phase_6+0x82>
+  401181:   eb 05                   jmp    401188 <phase_6+0x94>
+                                        !# 0x6032d0是链表头指针
+  401183:   ba d0 32 60 00          mov    $0x6032d0,%edx    !# 放入八个字节的 rdx
+
+  401188:   48 89 54 74 20          mov    %rdx,0x20(%rsp,%rsi,2) !#  rdx -> rsp[rsi *2 + 0x20 ] 
+  40118d:   48 83 c6 04             add    $0x4,%rsi       !# 0x20 - 0x50 
+  401191:   48 83 fe 18             cmp    $0x18,%rsi
+  401195:   74 14                   je     4011ab <phase_6+0xb7>
+                          !# 放入相对位置
+  401197:   8b 0c 34                mov    (%rsp,%rsi,1),%ecx  !# rsp[rsi] -> ecx
+  40119a:   83 f9 01                cmp    $0x1,%ecx           !# 如果这个数 <= 1 就去 401183，
+  40119d:   7e e4                   jle    401183 <phase_6+0x8f>
+  40119f:   b8 01 00 00 00          mov    $0x1,%eax           !# 否则回到 401176
+  4011a4:   ba d0 32 60 00          mov    $0x6032d0,%edx
+  4011a9:   eb cb                   jmp    401176 <phase_6+0x82>
+
+  4011ab:   48 8b 5c 24 20          mov    0x20(%rsp),%rbx     !#rbx最初结点
+  4011b0:   48 8d 44 24 28          lea    0x28(%rsp),%rax     !# rax是下一个点
+  4011b5:   48 8d 74 24 50          lea    0x50(%rsp),%rsi
+  4011ba:   48 89 d9                mov    %rbx,%rcx   !# rsp0x20
+
+    !# 这里相当于 nextArrayPos = rax, cur = rcx  然后让 cur ->next = array[nextArrayPos]
+    !# 也就是说这个链表呢， 被换成了 咱们数组中的顺序，这一段代码和本题无关！
+  4011bd:   48 8b 10                mov    (%rax),%rdx   !#  将数组中下一个点的值 赋给 rdx
+  4011c0:   48 89 51 08             mov    %rdx,0x8(%rcx)  !# rcx -> 数组中的下一个点
+  4011c4:   48 83 c0 08             add    $0x8,%rax       !# 换下一个地址
+  4011c8:   48 39 f0                cmp    %rsi,%rax
+  4011cb:   74 05                   je     4011d2 <phase_6+0xde>
+  4011cd:   48 89 d1                mov    %rdx,%rcx       !# rcx 后移
+  4011d0:   eb eb                   jmp    4011bd <phase_6+0xc9>
+
+    !# rdx 下来之后就指向最后一个结点 ， 让rdx ->next = NULL
+  4011d2:   48 c7 42 08 00 00 00    movq   $0x0,0x8(%rdx)
+  4011d9:   00 
+     !#  让每一个 都curArrayValue >= nextArrayValue  即 降序
+  4011da:   bd 05 00 00 00          mov    $0x5,%ebp 
+  4011df:   48 8b 43 08             mov    0x8(%rbx),%rax   !# rsp0x20 + 8 nextArrayValue = rax
+  4011e3:   8b 00                   mov    (%rax),%eax 
+  4011e5:   39 03                   cmp    %eax,(%rbx)      !# rbx 是 curArrayValue
+  4011e7:   7d 05                   jge    4011ee <phase_6+0xfa> !# 如果 curArrayValue >= nextArrayValue 就继续，否则爆炸
+  4011e9:   e8 4c 02 00 00          callq  40143a <explode_bomb>
+  4011ee:   48 8b 5b 08             mov    0x8(%rbx),%rbx
+  4011f2:   83 ed 01                sub    $0x1,%ebp        !# 循环5 - 1 + 1 =5 次 
+  4011f5:   75 e8                   jne    4011df <phase_6+0xeb>
+  4011f7:   48 83 c4 50             add    $0x50,%rsp     !# 恢复
+  4011fb:   5b                      pop    %rbx
+  4011fc:   5d                      pop    %rbp
+  4011fd:   41 5c                   pop    %r12
+  4011ff:   41 5d                   pop    %r13
+  401201:   41 5e                   pop    %r14
+  401203:   c3                      retq
